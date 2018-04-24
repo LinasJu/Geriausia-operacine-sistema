49e05226ffa07792b2006f62b082602d4d4f094f
brendonsa <brendonas.st@gmail.com>
2018-04-24 20:57:37 +0300

    Main

--- main.cpp
+++ main.cpp
@@ -8,12 +8,8 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    uint8_t aa=2;
-    uint8_t bb=2;
-    uint16_t cc=4;
-    uint16_t dd=4;
-    MainWindow w(&aa,&dd,&cc,&bb);
-    w.show();
-    w.update();
+    MainWindow *w= new MainWindow();
+    w->show();
+    w->update();
     return a.exec();
 }
